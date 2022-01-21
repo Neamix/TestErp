73d929cf3c3e85a88fba5dd52e6cd998359657d4
AbdalrhmanHussin12 <94906400+AbdalrhmanHussin12@users.noreply.github.com>
2022-01-21 13:19:24 +0200

    Add boot scope to teacher and student model

--- app/Student.php
+++ app/Student.php
@@ -2,9 +2,16 @@
 
 namespace App;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
 {
-    //
+    protected $table = 'users';
+
+    protected static function booted() {
+        static::addGlobalScope('students', function (Builder $builder) {
+            $builder->where('type', STUDENT);
+        });
+    }
 }
--- app/Teacher.php
+++ app/Teacher.php
@@ -2,9 +2,16 @@
 
 namespace App;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 class Teacher extends Model
 {
-    //
+    protected $table = 'users';
+
+    protected static function booted() {
+        static::addGlobalScope('teachers', function (Builder $builder) {
+            $builder->where('type', TEACHER);
+        });
+    }
 }
