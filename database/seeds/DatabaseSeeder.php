ccbc1e2f5281cc087558334a139ac9d85a151981
AbdalrhmanHussin12 <94906400+AbdalrhmanHussin12@users.noreply.github.com>
2022-01-29 16:18:03 +0200

    database seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        $this->call(UserSeeder::class);
         $this->call(PriviledgeSeeder::class);
         // $this->call(factory(Course::class)->times(50)->create());
     }
