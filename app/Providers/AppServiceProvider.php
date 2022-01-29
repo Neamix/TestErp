8bf419d39d71a09c8d0604f3a22d517cb34f0ae9
AbdalrhmanHussin12 <94906400+AbdalrhmanHussin12@users.noreply.github.com>
2022-01-29 23:02:55 +0200

    Disable Primary Key check for migrations

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,10 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Events\MigrationsEnded;
+use Illuminate\Database\Events\MigrationsStarted;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Event;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +17,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        Event::listen(MigrationsStarted::class, function () {
+            DB::statement('SET SESSION sql_require_primary_key=0');
+        });
+        Event::listen(MigrationsEnded::class, function () {
+            DB::statement('SET SESSION sql_require_primary_key=1');
+        });
     }
 
     /**
