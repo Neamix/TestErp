55dd0e86e964b4606f521ca68d5ab79fc643c933
AbdalrhmanHussin12 <94906400+AbdalrhmanHussin12@users.noreply.github.com>
2022-01-29 16:12:50 +0200

    edit priviledge seeder

--- database/seeds/PriviledgeSeeder.php
+++ database/seeds/PriviledgeSeeder.php
@@ -15,98 +15,84 @@ class PriviledgeSeeder extends Seeder
         Priviledge::updateOrCreate([
             'id' => 1,
             'constant' => 'VIEW_USER_LIST',
-            'parent_id' => null,
             'name' => 'View User List'
         ]);
 
         Priviledge::updateOrCreate([
             'id' => 2,
             'constant' => 'EDIT_USER',
-            'parent_id' => VIEW_USER_LIST,
             'name' => 'Edit User'
         ]);
 
         Priviledge::updateOrCreate([
             'id' => 3,
             'constant' => 'DELETE_USER',
-            'parent_id' => VIEW_USER_LIST,
             'name' => 'Delete User'
         ]);
 
         Priviledge::updateOrCreate([
             'id' => 4,
             'constant' => 'SUSBEND_USER',
-            'parent_id' => VIEW_USER_LIST,
             'name' => 'Susbend User'
         ]);
 
         Priviledge::updateOrCreate([
             'id' => 5,
             'constant' => 'TRASH_USER',
-            'parent_id' => VIEW_USER_LIST,
             'name' => 'Trash User'
         ]);
 
         Priviledge::updateOrCreate([
             'id' => 6,
             'constant' => 'VIEW_USER_PRIVILEDGES',
-            'parent_id' => null,
             'name' => 'View User Priviledges'
         ]);
 
         Priviledge::updateOrCreate([
             'id' => 7,
             'constant' => 'EDIT_USER_PRIVILEDGES',
-            'parent_id' => VIEW_USER_PRIVILEDGES,
             'name' => 'Edit User Priviledges'
         ]);
 
         Priviledge::updateOrCreate([
             'id' => 8,
             'constant' => 'REMOVE_USER_PRIVILEDGES',
-            'parent_id' => VIEW_USER_PRIVILEDGES,
             'name' => 'Remove User Priviledges'
         ]);
 
         Priviledge::updateOrCreate([
             'id' => 9,
             'constant' => 'SUPER_ADMIN',
-            'parent_id' => null,
             'name' => 'Super Admin'
         ]);
 
         Priviledge::updateOrCreate([
             'id' => 10,
             'constant' => 'SYSTEM_ADMIN',
-            'parent_id' => null,
             'name' => 'SYSTEM Admin'
         ]);
 
         Priviledge::updateOrCreate([
             'id' => 11,
             'constant' => 'VIEW_SUBJECT_LIST',
-            'parent_id' => null,
             'name' => 'View subject list'
         ]);
 
         Priviledge::updateOrCreate([
             'id' => 12,
             'constant' => 'DELETE_SUBJECT',
-            'parent_id' => VIEW_SUBJECT_LIST,
             'name' => 'Delete subject'
         ]);
 
         Priviledge::updateOrCreate([
             'id' => 13,
             'constant' => 'EDIT_SUBJECT',
-            'parent_id' => VIEW_SUBJECT_LIST,
             'name' => 'Edit subject'
         ]);
 
         Priviledge::updateOrCreate([
             'id' => 14,
             'constant' => 'TRASH_SUBJECT',
-            'parent_id' => VIEW_SUBJECT_LIST,
             'name' => 'Trash subject'
         ]);
     }
